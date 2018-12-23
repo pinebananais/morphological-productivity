a43a5b2fa22541028d5c33d8f0e2b0e1e8b77caa
황보 정 <bomul0327@naver.com>
2018-12-24 01:33:55 +0900

    modify output format

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 import morph_config as mc
 from nltk.tokenize import word_tokenize
+import sys
+
+def printProgress (iteration, total, prefix = '', suffix = '', decimals = 1, barLength = 100):
+    formatStr = "{0:." + str(decimals) + "f}"
+    percent = formatStr.format(100 * (iteration / float(total)))
+    filledLength = int(round(barLength * iteration / float(total)))
+    bar = '#' * filledLength + '-' * (barLength - filledLength)
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percent, '%', suffix)),
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
+
 
 def suffix_code(s):
 	for suffix in mc.suffix_table:
@@ -10,18 +22,28 @@ def suffix_code(s):
 def contribution(count):
 	return count / (2 ** count)
 
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
+
 count_table = {}
 
 for suffix in mc.suffix_table:
 	count_table[suffix] = {}
 
+total_idx = sum(1 for line in open(mc.file_name, encoding = 'utf-8'))
+word_cnt = 0
 with open(mc.file_name, 'r', encoding = 'utf-8') as lines:
 	idx = 0
 	for line in lines:
+		idx += 1
+		if idx % 1000 == 0: printProgress(idx, total_idx, 'Progress:', 'Complete', 1, 50)
 		line = line.lower()
 		tokens = word_tokenize(line)
 		for token in tokens:
-			idx += 1
+			word_cnt += 1
 			suffix = suffix_code(token)
 			if suffix in mc.suffix_table:
 				if token in count_table[suffix]:
@@ -31,13 +53,34 @@ with open(mc.file_name, 'r', encoding = 'utf-8') as lines:
 					count_table[suffix][token] = 1
 					count_table[suffix]["__TOTAL__"] = 1
 
+col_len = 12
+if mc.flag_1: col_len += 12
+if mc.flag_2: col_len += 12
+
+print("")
+print("Total %d words in dataset." % word_cnt)
+for i in range(col_len):
+	print("=", end="")
+print("")
+print("|   suffix   |", end="")
+if mc.flag_1: print("  method1 |", end="")
+if mc.flag_2: print("  method2 |", end="")
+print("")
+for i in range(col_len):
+	print("=", end="")
+print("")
 for suffix in mc.suffix_table:
-	P = 0
+	P1 = 0
 	P2 = 0
 	for key, val in count_table[suffix].items():
 		if key == "__TOTAL__": continue
 		if val == 1 : P2 += 1
-		P += contribution(val)
+		P1 += contribution(val)
 	P2 /= count_table[suffix]["__TOTAL__"]
-	print("%s : %f" % (suffix, P))
-	print("%s : %f" % (suffix, P2))
+	print("|-%-11s|" % suffix, end="")
+	if mc.flag_1: print("%10.2f|" % P2, end="")
+	if mc.flag_2: print("%10.2f|" % P1, end="")
+	print("")
+for i in range(col_len):
+	print("=", end="")
+print("")
--- morph_config.py
+++ morph_config.py
@@ -1,8 +1,8 @@
 # list of suffixes to trace
-suffix_table = ['ly', 'ous', 'ment'] # can append new suffixes
+suffix_table = ['ly', 'ous', 'ment', 'ity', 'ful', 'ic', 'ship'] # can append new suffixes
 
 # file name store raw data(in relative path)
-file_name = "./data/sample_raw_data.txt"
+file_name = "./data/small_raw_data.txt"
 
 # flag for using original method based on hapax legomenon (True for use)
 flag_1 = True # [True | False]
